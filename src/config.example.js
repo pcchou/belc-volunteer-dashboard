5516a2b2d79712997927ca61aaa75d41dd1af05f
Ping-yu Chou <mail@pcchou.me>
2023-09-18 16:42:16 +0800

    Fix config example

--- src/config.example.js
+++ src/config.example.js
@@ -1,1 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/PUT_URL_HERE/exec";
+let config = {};
+config.SCRIPT_URL = "https://script.google.com/macros/s/PUT_URL_HERE/exec";
+
+module.exports = config;
